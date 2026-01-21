8fd89cc5ad22227ee0404e22a57ca752455716f5
Gregorian Rayne <gregorianrayne09@gmail.com>
2026-01-21 23:53:10 +0100

    feat(version): add version information structure
    Define version constants for library versioning with both preprocessor macros and compile-time struct access

---
+++ headers/pnf/version.hpp
@@ -0,0 +1,24 @@
+//
+// Created by gregorian-rayne on 12/01/2026.
+//
+
+#ifndef VERSION_HPP
+#define VERSION_HPP
+
+#define PNF_VERSION_MAJOR 0
+#define PNF_VERSION_MINOR 1
+#define PNF_VERSION_PATCH 0
+#define PNF_VERSION_STRING "0.1.0"
+
+namespace pnf {
+
+    struct Version {
+        static constexpr int major = PNF_VERSION_MAJOR;
+        static constexpr int minor = PNF_VERSION_MINOR;
+        static constexpr int patch = PNF_VERSION_PATCH;
+        static constexpr auto string = PNF_VERSION_STRING;
+    };
+
+}
+
+#endif //VERSION_HPP
